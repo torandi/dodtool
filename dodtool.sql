22f21f58f3ab67b23980f709845209ebfd5b784f
Andreas Tarandi <torandi@gmail.com>
2012-08-05 01:18:37 +0200

    Add sql dump

---
+++ dodtool.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 05, 2012 at 01:17 AM
+-- Server version: 5.5.24
+-- PHP Version: 5.3.10-1ubuntu3.2
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `dodtool`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `battles`
+--
+
+CREATE TABLE IF NOT EXISTS `battles` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) COLLATE utf8_swedish_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `entities`
+--
+
+CREATE TABLE IF NOT EXISTS `entities` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `battle_id` int(10) unsigned NOT NULL,
+  `name` varchar(32) COLLATE utf8_swedish_ci NOT NULL,
+  `life` int(10) unsigned NOT NULL,
+  `life_remaining` int(11) NOT NULL,
+  `armor` int(10) unsigned NOT NULL,
+  `armor_type` enum('normal','natural') COLLATE utf8_swedish_ci NOT NULL DEFAULT 'normal',
+  `type` enum('friendly','neutral','hostile') COLLATE utf8_swedish_ci NOT NULL DEFAULT 'hostile',
+  `state` enum('normal','unconscious','dead') COLLATE utf8_swedish_ci NOT NULL DEFAULT 'normal',
+  `color` varchar(32) COLLATE utf8_swedish_ci NOT NULL,
+  `visible` tinyint(1) NOT NULL DEFAULT '0',
+  `info` text COLLATE utf8_swedish_ci NOT NULL,
+  `position_x` int(11) NOT NULL,
+  `position_y` int(11) NOT NULL,
+  `rotation` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `battle_id` (`battle_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=1 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `entities`
+--
+ALTER TABLE `entities`
+  ADD CONSTRAINT `entities_ibfk_1` FOREIGN KEY (`battle_id`) REFERENCES `battles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
