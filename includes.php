4e811252b20c8ff5ed98a29b98518d480871f4b8
Andreas Tarandi <torandi@gmail.com>
2012-08-04 17:44:54 +0200

    Initial commit

---
+++ includes.php
@@ -0,0 +1,27 @@
+<?php
+$repo_root=dirname(__FILE__);
+
+include "settings.php";
+
+/**
+ * Automatiskt anropad av php-5 on-demand för att include:a filer med klassdefinitioner.
+ * Sätter den globala variabern $repo_root till sökvägen till svn-repots root-mapp.
+ */
+function __autoload($class)
+{
+	global $repo_root;
+	if(file_exists($repo_root.'/classes/'.$class.'.php')){
+		require_once $repo_root.'/classes/'.$class.'.php';
+	}
+}
+
+session_start();
+
+/* Database */
+$db = new mysqli(
+	$db_settings['host'],
+	$db_settings['username'],
+	$db_settings['password'],
+	$db_settings['database'],
+	$db_settings['port']
+);
---
+++ index.php
@@ -0,0 +1,29 @@
+<?
+	include "includes.php";
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sv" lang="sv">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>DoD Spelledarverktyg</title>
+		<script src="jquery.js" type="text/javascript"></script>
+		<script src="dod.js" type="text/javascript"></script>
+		<script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
+		<script type="text/javascript" src="js/toggle_display.js"></script>
+		<script type="text/javascript" src="js/forum.js"></script>
+		<script type="text/javascript" src="js/wz_jsgraphics.js"></script>
+		<script type="text/javascript" src="js/override_chrome_stupidity.js"></script>
+		
+		<link type="text/plain" rel="author" href="/humans.txt" />
+	</head>
+	<body>
+	<div id="battle_select">
+		<h2>Ändra/skapa strid</h2>
+		<form method="get">
+			<label for="battle_id">Befintlig strid:</label>
+			<select id="battle_id" name="id">
+				
+			</select>
+	</div>
+	</body>
+</html>
